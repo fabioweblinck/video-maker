9d2210a6e816a9668c774d11355538f0c8a391cf
fabioweblinck <fabio_alencar79@hotmail.com>
2020-01-10 09:55:52 -0400

    feat: start text robot

---
+++ credentials/README.md
@@ -0,0 +1,11 @@
+# Credentials format
+
+## Algorithmia
+
+File: `algorithmia.json`
+
+```
+{
+	"apiKey": "simiFYVU/VzrDVfGT3BTjNYAaS81"
+}
+```
--- index.js
+++ index.js
@@ -1,24 +1,29 @@
 const readline = require('readline-sync')
+const robots = {
+  text: require('./robots/text.js')
+}
 
-function start() {
+async function start() {
   const content = {}
   
   content.searchTerm = askAndReturnSearchTerm()
   content.prefix = askAndReturnPrefix()
 
+  await robots.text(content)
+
   function askAndReturnSearchTerm() {
-	return readline.question('Type a Wikipedia searchterm: ')
+  return readline.question('Type a Wikipedia searchterm: ')
   }
 
   function askAndReturnPrefix() {
-  	const prefixes = ['Who is', 'What is', 'The history of']
-  	const selectedPrefixIndex = readline.keyInSelect(prefixes, 'choose one option: ')
-  	const selectedPrefixText =prefixes[selectedPrefixIndex]
+    const prefixes = ['Who is', 'What is', 'The history of']
+    const selectedPrefixIndex = readline.keyInSelect(prefixes, 'choose one option: ')
+    const selectedPrefixText =prefixes[selectedPrefixIndex]
 
-  	return selectedPrefixText
+    return selectedPrefixText
   }
 
-  console.log(content)
+ console.log(content)
 }
 
 start()
